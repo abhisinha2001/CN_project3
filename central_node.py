4b82841726e29b2a11bf1b7d8c4f3a8fd2f7f24a
Abhinav Sinha <abhinavsinha@Abhinavs-MacBook-Air.local>
2022-04-07 18:02:21 +0100

    Resolved some issues and added timeout functionality

--- central_node.py
+++ central_node.py
@@ -6,7 +6,8 @@ import sys    # Required for exiting loops based on keyboard interrupt
 PORT = 33000
 MCAST_GRP = '224.1.1.1'     # Multicast group used to send a UDP broadcast over a specific group 
 # MCAST_PORT = 34000           # Port for the mulsticast to run on
-MULTICAST_TTL = 2
+MULTICAST_TTL = 3
+time_start = time.localtime(time.time()) 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # seperate socket for UDP Multicasting
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)  # Providing socket parameters to define socket behaviour
@@ -18,17 +19,24 @@ while True:
         s.connect((socket.gethostname(),PORT))
         # s.connect(('10.35.70.12',PORT))              # Connecting with the server socket
         data = pickle.loads(s.recv(1024))                   # Recieving data and loading it as byte stream using pickle
-        Nitrogen = data["Nitrogen level"]                               # Destructuring of data
-        Phosphate = data["Phosphate Level"]
-        PH = data["PH"]                    
-        print(f"Nitrogen Level:{Nitrogen} Phosphate Level: {Phosphate} PH Level:{PH}") # Console message
-        # data["source"] = ""     # Altering the source for further relay
-        for MCAST_PORT in range(34000,34005):
-            sock.sendto(pickle.dumps(data), (MCAST_GRP, MCAST_PORT)) # Using the UDP broadcasting socket to broadcast the recieved data
-        time.sleep(1)                           # Performing a request every second
+        if data:
+            Nitrate = data["Nitrate level"]                               # Destructuring of data
+            Phosphate = data["Phosphate Level"]
+            Source  = data["Source"]                   
+            time_result = time.localtime(time.time())         
+            print(f"Time: {time_result.tm_hour}:{time_result.tm_min}:{time_result.tm_sec} \nNitrate Level:{Nitrate} \nPhosphate Level: {Phosphate} \nSource: {Source}\n")# Console message
+            data["Source"] = "Central Node"     # Altering the source for further relay
+            time_start = time.localtime(time.time())
+            for MCAST_PORT in range(34000,34005):
+                sock.sendto(pickle.dumps(data), (MCAST_GRP, MCAST_PORT)) # Using the UDP broadcasting socket to broadcast the recieved data
+        if time_start.tm_hour - time.localtime(time.time()).tm_hour >1:
+            print("\nTIMEOUT ALERT. Please check node 2\n")
+                
+        time.sleep(2)                           # Performing a request every second
     except KeyboardInterrupt:                   # Detect Keyboard event
         sys.exit(0)                             # Exit program
     
 
 s.close()
+sock.close()
 
--- server.py
+++ server.py
@@ -5,11 +5,11 @@ import sys    # Required for exiting loops based on keyboard interrupt
 
 PORT = 33000    #Arbitrary port number assigned for socket communication (Usually above 5000)
 
-Levels = [{"Nitrogen level":1290,"Phosphate Level":1900,"PH":6.6},{"Nitrogen level":2200,"Phosphate Level":1800,"PH":7.1},{"Nitrogen level":1314,"Phosphate Level":1800,"PH":7.1},{"Nitrogen level":1400,"Phosphate Level":2100,"PH":8.4}] #Testing CO2 values in ppm(parts per million)
+Levels = [{"Nitrate level":1250,"Phosphate Level":1900,"Source":"Irrigation Server"},{"Nitrate level":1300,"Phosphate Level":1800,"Source":"Irrigation Server"},{"Nitrate level":1350,"Phosphate Level":1800,"Source":"Irrigation Server"},{"Nitrate level":1400,"Phosphate Level":2100,"Source":"Irrigation Server"}] #Testing values in ppm(parts per million)
 
 s= socket.socket(socket.AF_INET,socket.SOCK_STREAM) # Creating a socket that supports TCD connection
 s.bind((socket.gethostname(),PORT))                 # Binding the socket to a the previosly declared port
-s.listen(5)                                         #Listening for connections
+s.listen(5)                                         # Listening for connections
 
 while True:                                         
     data = Levels[random.randint(0, len(Levels)-1)] # Produce a random value from the Levels array
